9db2326e7dffe0846a18aaad111bdca229c953a6
Bernat <bernatpavon@gmail.com>
2022-08-05 09:25:08 +0200

    add: typed auth middleware

---
+++ server/middlewares/auth.ts
@@ -0,0 +1,20 @@
+const User = require('./../model/user.model');
+import { Request, Response, NextFunction } from 'express';
+
+const authMiddleware = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	try {
+		const { id } = req.session;
+		const user = await User.findOne({ _id: id });
+		if (!user) throw new Error();
+		req.body.user = user;
+		next();
+	} catch (e) {
+		res.sendStatus(404);
+	}
+};
+
+module.exports = authMiddleware;
