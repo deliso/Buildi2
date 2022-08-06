a15bde50a706eca567e09b29a91355206ed1f201
deliso <s.moraleslopez.bcn@gmail.com+>
2022-08-06 09:19:50 +0200

    build(fe root): disable eslint (eslintrc.json)

---
+++ server/model/project.model.ts
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+import { Bid, RFI, ProjectT } from '../../types/projectTypes';
+
+const { Schema } = mongoose;
+
+const bidSchema = new Schema<Bid>({
+	bidPrice: Number,
+	creatorId: String,
+	creatorName: String,
+	creatorPic: String,
+	awarded: Boolean,
+});
+
+const RFISchema = new Schema<RFI>({
+	question: String,
+	response: String,
+	creatorId: String,
+	creatorPic: String,
+});
+
+const projectSchema = new Schema<ProjectT>({
+	projectImage: {
+		type: String,
+		required: false,
+	},
+	name: String,
+	description: String,
+	userId: String,
+	specialties: [String],
+	lifeCycle: String,
+	bids: [bidSchema],
+	rfis: [RFISchema],
+});
+
+const Project = mongoose.model('project', projectSchema)
+
+export default Project;
+// module.exports.Project = mongoose.model('project', projectSchema);
